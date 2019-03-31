ea13c52d160ae3617fa53d706e5623a7c91b32c6
Luis Henrique Witz <luiswitz@gmail.com>
2019-03-31 00:23:40 +0000

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_001805) do
+ActiveRecord::Schema.define(version: 2019_03_31_002134) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "kind"
     t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
